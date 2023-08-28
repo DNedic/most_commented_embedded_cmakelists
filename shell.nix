bcfc53fe2ca28ab46a50f20ac1eb775c48966c30
Djordje Nedic <nedic.djordje2@gmail.com>
2023-08-29 00:03:31 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
+  }) {};
+
+in pkgs.mkShell {
+  packages = [
+    pkgs.gcc-arm-embedded
+    pkgs.cmake
+    pkgs.gnumake
+    pkgs.git
+    pkgs.openocd
+  ];
+}
