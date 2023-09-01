5047c7a6b091f6a3ca20d32a28f7a5a1b8f2aaf0
Djordje Nedic <nedic.djordje2@gmail.com>
2023-09-01 02:27:07 +0200

    Replace wrong occurrence of builtins.fetchGit with builtins.fetchTarball in nix.shell

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import (builtins.fetchGit {
+  pkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
   }) {};
 
